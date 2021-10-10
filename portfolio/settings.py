f30a3a8e702c1f7aceefb0a6dfacdfc6e537e1c0
juned <junedaslam96@gmail.com>
2021-10-10 10:19:03 +0530

    added static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
+STATIC_ROOT =os.join(BASE_DIR ,'staticfile')
 
 
 # Default primary key field type
