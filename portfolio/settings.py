f22b15b240daedf5eb7f3b852ca0d0e371d46e9f
juned aslam <junedaslam96@gmail.com>
2022-08-26 22:30:33 +0530

    change setting file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7_9l+d$xd1wntg-vo+&+8)t#79l)(6#wxj2ehele)mz*88)6+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'junedportfolio.heroku.com'
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =os.path.join(BASE_DIR ,'staticfile')
+STATIC_ROOT =os.path.join(BASE_DIR ,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
